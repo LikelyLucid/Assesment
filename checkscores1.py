300bc77172415438f8da3ccc898619115c746548
PurelyStupidity <micoolplays@gmail.com>
2021-08-16 10:03:54 +1200

    August 16, 2021 10:03 AM

--- checkscores1.py
+++ checkscores1.py
@@ -10,9 +10,9 @@ def check_score(player_score):
         code = False
     return code
 
-# if check_score(int(input("Waht is the player score:"))):
-#     print("Continue")
-# else:
-#     print("Sorry, you have no lives left")
-#     print("{final summary}")
+if check_score(int(input("Waht is the player score:"))):
+    print("Continue")
+else:
+    print("Sorry, you have no lives left")
+    print("{final summary}")
 
