b8f60dcda0c2e64f004896bf6e267cc36485f4ea
PurelyStupidity <micoolplays@gmail.com>
2021-08-24 11:45:22 +1200

    August 24, 2021 11:45 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -8,6 +8,6 @@ def SortList():
     with open("./Scores.txt", "w") as scores:
         for line in scores:
             list.append(line)
-    print(sorted(list, key=get_first_value))
+    # print(sorted(list, key=get_first_value))
     return sorted(list, key=get_first_value)
 SortList()
