144a79f768c844967d319f529aa8da018af4fd03
PurelyStupidity <micoolplays@gmail.com>
2021-08-24 11:47:46 +1200

    August 24, 2021 11:47 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -10,4 +10,5 @@ def SortScores():
             list.append(line)
     # print(sorted(list, key=get_first_value))
     return sorted(list, key=get_first_value)
-print(SortScores())
+#print(SortScores())
+
