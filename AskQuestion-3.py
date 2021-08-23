4abb8f673d181c34561e349a27e1a2c1c3b66a9b
PurelyStupidity <micoolplays@gmail.com>
2021-08-24 11:04:24 +1200

    August 24, 2021 11:04 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -26,7 +26,7 @@ def finish():
 def answerright(points):
     print("Correct answer!")
     points = points + 1 # add 1 to score
-    print("your score is: ", points - 1) # minus 1 point because they start with 1 point, Easier than setting score to 
+    print("your score is: ", points - 1) # minus 1 point because they start with 1 point, Easier than setting score to 0 and checking if s
     return points
 
 
