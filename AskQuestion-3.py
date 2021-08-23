4e21d09510e9e6bcc09ac4d9dbe753797ae6e327
PurelyStupidity <micoolplays@gmail.com>
2021-08-24 11:00:18 +1200

    August 24, 2021 11:00 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -34,6 +34,7 @@ def answerwrong(points):
     print("answer wrong")
     print(f"The answer was {answer}")
     points = points - 1
+    
     print("your score is: ", points - 1)
     return points
 
