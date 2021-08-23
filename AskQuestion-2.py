1e31c3fb15b89edcd86fc3f2a1ce0dfaec46c02b
PurelyStupidity <micoolplays@gmail.com>
2021-08-23 13:57:41 +1200

    August 23, 2021 1:57 PM

--- AskQuestion-2.py
+++ AskQuestion-2.py
@@ -54,7 +54,12 @@ while True:
                 answer = num1 + num2
             # ask for answer and check if it is an integer
             ask = integer_checker("What does\n{} + {} = ".format(num1, num2))
-            points = answerright(points) if ask == answer else answerwrong(points)
+            if ask == answer:
+                points = answerright(points)
+
+            else:
+                points = answerwrong(points)
+
         elif choice == 2:
             # figure out the answer
             answer = num1 - num2
@@ -63,7 +68,12 @@ while True:
                 answer = num1 - num2
             # ask for answer through interger checker
             ask = integer_checker("What does\n{} - {} = ".format(num1, num2))
-            points = answerright(points) if ask == answer else answerwrong(points)
+            if ask == answer:
+                points = answerright(points)
+
+            else:
+                points = answerwrong(points)
+
     elif difficulty == "hard":
         hardmin, hardmax = hardmin+(points * 5), hardmax+(points * 5)
         # Set numbers to random a random number
@@ -77,16 +87,30 @@ while True:
             answer = num1 + num2
             # ask for answer and check if it is an integer
             ask = integer_checker("What does\n{} + {} = ".format(num1, num2))
-            points = answerright(points) if ask == answer else answerwrong(points)
+            if ask == answer:
+                points = answerright(points)
+
+            else:
+                points = answerwrong(points)
+
         elif choice == 2:
             # figure out the answer
             answer = num1 - num2
             # ask for answer through interger checker
             ask = integer_checker("What does\n{} - {} = ".format(num1, num2))
-            points = answerright(points) if ask == answer else answerwrong(points)
+            if ask == answer:
+                points = answerright(points)
+
+            else:
+                points = answerwrong(points)
+
         elif choice == 4:
             # figure out the answer
             answer = num1 * num2
             # ask for answer through interger checker
             ask = integer_checker("What does\n{} * {} = ".format(num1, num2))
-            points = answerright(points) if ask == answer else answerwrong(points)
+            if ask == answer:
+                points = answerright(points)
+
+            else:
+                points = answerwrong(points)
