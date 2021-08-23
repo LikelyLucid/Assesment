d99f88f362277836efd3e1d4a48ebce57f478c18
PurelyStupidity <micoolplays@gmail.com>
2021-08-24 11:03:01 +1200

    August 24, 2021 11:03 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -37,7 +37,7 @@ def answerwrong(points):
     if check_score(points): #check if score is 0
         finish() #if so call the end code
     print("your score is: ", points - 1)
-    return points #return
+    return points #return the points
 
 
 while True:
