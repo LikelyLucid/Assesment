83e2cdc43eaed241241f2627fd158f048f8347e3
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 09:47:34 +1200

    August 25, 2021 9:47 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -60,7 +60,7 @@ def answerwrong(points):
 while True:
     if difficulty == "Easy":
         # Set numbers to random a random number
-        num1 = random.randint(easymin, easymax + (rounds * 2))
+        num1 = random.randint(easymin, easymax + (rounds * 3))
         num2 = random.randint(easymin, easymax)
         # Make a choice between + or -
         choice = random.randint(1, 2)
