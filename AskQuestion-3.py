ca697d62784546f098dbf13d5771b8d858487d76
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 09:14:14 +1200

    August 25, 2021 9:14 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -137,7 +137,7 @@ while True:
                 points = answerwrong(points)
 
         else:
-            num1, num2 = random.randint(hardmin, hardmax)
+            num1 = random.randint(hardmin, hardmax)
             # figure out the answer
             answer = num1 * num2
             if cheatermode:
