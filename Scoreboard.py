c206dc3aff1dca813df95d5a9a0dcdeff811a6bd
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:11:51 +1200

    August 25, 2021 8:11 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -3,8 +3,7 @@ list= []
 def get_first_value(string):
     print(string.split("-")[0])
     return int(string.split("-")[0])
-def removenewlines(string):
-    return string.replace("\n", "")
+
 
 def SortScores():
     # print(sorted(list, key=get_first_value))
@@ -13,7 +12,7 @@ def SortScores():
 def add_to_Scoreboard(string):
     with open("./Scores.txt", "r") as scores:
         for line in scores:
-            list.append(removenewlines(line.replace("\n", "")))
+            list.append(line.replace("\n", "")))
         list.append(string)
 
 add_to_Scoreboard("23 - Michael")
