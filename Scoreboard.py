09860b30b8f98b9c7c78b95e47fd43a35fa09840
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:16:29 +1200

    August 25, 2021 8:16 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -13,7 +13,7 @@ def SortScores():
     return sorted(list, key=get_first_value, reverse=True)
 
 def add_to_Scoreboard(string):
-    with open("./Scores.txt", "r") as scores:
+    with open("./Scores.txt", "r+") as scores:
         for line in scores:
             list.append(line.replace("\n", ""))
         list.append(string)
