0009abf5a6187580cb83265207adc5620ca66e3b
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:15:03 +1200

    August 25, 2021 8:15 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -3,7 +3,10 @@ list= []
 def get_first_value(string):
     print(string.split("-")[0])
     return int(string.split("-")[0])
-def emptyfile(file)
+
+
+def emptyfile(file):
+    
 
 def SortScores():
     # print(sorted(list, key=get_first_value))
