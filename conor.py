2dd95ee191d2caac958a89740c908f17966dbcb1
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 10:48:04 +1200

    August 25, 2021 10:48 AM

--- conor.py
+++ conor.py
@@ -66,7 +66,7 @@ def hard_quiz():
             total_correct += 1
         else:
             print("Incorrect")
-            print("answer is")
+            print("answer was:")
             print(hard_compare)
     return total_correct
 
