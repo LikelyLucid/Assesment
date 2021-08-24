e734269edeb808a6dc3cbecb0771774e1251fb4c
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:20:03 +1200

    August 25, 2021 8:20 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -17,7 +17,8 @@ def add_to_Scoreboard(string):
         for line in scores:
             list.append(line.replace("\n", ""))
         list.append(string)
-    emptyfile("./Scores.txt"
+    emptyfile("./Scores.txt")
+    
     for i in list:
         scores.write(list[i - 1], "\n")
 
