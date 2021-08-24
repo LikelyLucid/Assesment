aa6af091e40eb0671182f5c379da4f4416741a9f
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:39:23 +1200

    August 25, 2021 8:39 AM

--- checkscores1.py
+++ checkscores1.py
@@ -1,6 +1,12 @@
 # check player scores
 def check_score(player_score):
-    return player_score > 0
+    # if score is more than 0 return true so that i can do "if check_score:"
+    if player_score > 0:
+        code = True
+    #if not return False
+    else:
+        code = False
+    return code
 
 """
 Used for testing:
