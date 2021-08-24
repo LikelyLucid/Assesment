beb705ff4ba09486659893e99a1b4c64d419be64
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 11:00:05 +1200

    August 25, 2021 11:00 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -18,7 +18,7 @@ name, difficulty = playerInformation()
 def integer_checker(question):
     error = "\nSorry, you must enter a number\n"
     number = ""
-    if 
+    if question ==
     while not number:
         try:
             number = int(input(question))
