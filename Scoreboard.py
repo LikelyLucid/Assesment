ee69c37ee95c514447be932b6284584a3bdbe856
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 07:56:52 +1200

    August 25, 2021 7:56 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -6,9 +6,6 @@ def get_first_value(string):
 
 def SortScores():
     list = []
-    
-    for line in scores:
-        list.append(line)
     # print(sorted(list, key=get_first_value))
     return sorted(list, key=get_first_value, reverse=True)
 
