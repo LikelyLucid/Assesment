c318b210352a74688ea6b175fc75b784071e2420
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 09:12:46 +1200

    August 25, 2021 9:12 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -137,6 +137,7 @@ while True:
                 points = answerwrong(points)
 
         else:
+            
             # figure out the answer
             answer = num1 * num2
             if cheatermode:
