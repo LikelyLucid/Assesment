996678a8071d0c4aa4a549549defd0330f5b204c
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 09:19:10 +1200

    August 25, 2021 9:19 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -141,7 +141,7 @@ while True:
                 points = answerwrong(points)
 
         else:
-            num1 = random.randint(3, (rounds * 3))
+            num1 = random.randint(3, 1 +(rounds * 3))
             num2 = random.randint(3, (rounds * 3))
             # figure out the answer
             answer = num1 * num2
