e952900b5f66d9a1d301ff2463d022efc4b31b1b
PurelyStupidity <micoolplays@gmail.com>
2021-08-24 13:24:41 +1200

    August 24, 2021 1:24 PM

--- PlayerInformation-1.py
+++ PlayerInformation-1.py
@@ -19,9 +19,7 @@ difficulty = input("What difficulty would you like to play with?\nEasy or Hard: 
 
 # Check if the user has entered easy or hard. if not, ask them again.
 while True:
-    if difficulty == "Easy":
-        break
-    elif difficulty == "Hard":
+    if difficulty in ["Easy", "Hard"]:
         break
     difficulty = input("Please enter a difficulty\nEasy or Hard: ").capitalize()
 
