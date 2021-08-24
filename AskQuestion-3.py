21d043604d2a6bed8d304c451852f85a421b892b
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 09:47:53 +1200

    August 25, 2021 9:47 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -61,7 +61,7 @@ while True:
     if difficulty == "Easy":
         # Set numbers to random a random number
         num1 = random.randint(easymin, easymax + (rounds * 3))
-        num2 = random.randint(easymin, easymax)
+        num2 = random.randint(easymin, easymax + (rounds * 3) )
         # Make a choice between + or -
         choice = random.randint(1, 2)
 
