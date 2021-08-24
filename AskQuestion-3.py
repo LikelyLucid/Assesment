fec405048fe1b76ae568dbadee726a6cbe5ca208
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 09:16:17 +1200

    August 25, 2021 9:16 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -125,7 +125,6 @@ while True:
         
         elif choice == 3:
             num1 = random.randint(3, rounds * 3) #change the difficulty of the numbers because when testig tey were to hard to solve,
-            
             num2 = random.randint(3, rounds * 3)
             # figure out the answer
             answer = num1 / num2
