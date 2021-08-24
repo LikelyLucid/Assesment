511cef50979ebc7548a91b5ca37131a0d8ee6328
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 09:38:58 +1200

    August 25, 2021 9:38 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -30,7 +30,7 @@ def finish():
     global rounds
     print("your score was: ", points - 1)
     print(f"you lasted {rounds} rounds")
-    add_to_Scoreboard(f"{rounds} - {name} - {difficulty}")
+    add_to_Scoreboard(f"{rounds} - {difficulty} - {name} ")
     exit()
 
 
