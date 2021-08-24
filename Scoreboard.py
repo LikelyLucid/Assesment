44389d96cd858fec17213137ec6b93f6d771d1c8
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:02:56 +1200

    August 25, 2021 8:02 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -9,9 +9,9 @@ def SortScores():
     return sorted(list, key=get_first_value, reverse=True)
 
 def add_to_Scoreboard(string):
-    with open("./Scores.txt", "a+") as scores:
-        scores.seek(0)
-
+    with open("./Scores.txt", "r") as scores:
+        for line in scores:
+            list.append(line)
         scores.write(string)
 add_to_Scoreboard("23 - Michael")
 scores = SortScores()
