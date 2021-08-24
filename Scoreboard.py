7102cf7be273f0066df2a182cb79c926b62614b9
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:02:33 +1200

    August 25, 2021 8:02 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,21 +1,21 @@
-global list 
-list= []
+global list = []
 def get_first_value(string):
     print(string.split("-")[0])
     return int(string.split("-")[0])
 
 def SortScores():
+    list = []
+    with open("./Scores.txt", "r") as scores:
+        for line in scores:
+            list.append(line)
     # print(sorted(list, key=get_first_value))
     return sorted(list, key=get_first_value, reverse=True)
 
 def add_to_Scoreboard(string):
     with open("./Scores.txt", "a+") as scores:
-        
-        for line in scores:
-            list.append(line)
-        print(list)
-
         scores.seek(0)
+
+        scores.write(string)
 add_to_Scoreboard("23 - Michael")
 scores = SortScores()
 print(scores)
