63cb7cd9486336c1ee549f75d76f6877814399de
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 11:02:22 +1200

    August 25, 2021 11:02 AM

--- PlayerInformation1.py
+++ PlayerInformation1.py
@@ -13,7 +13,7 @@ def playerInformation():
     print("Hello, " + name.capitalize())
     print("Welcome to my maths quiz")
     print("Important information:")
-    pr
+    print("")
 
 
     difficulty = input("What difficulty would you like to play with?\nEasy or Hard: ").capitalize()
