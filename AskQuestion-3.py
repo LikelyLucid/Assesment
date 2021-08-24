aab12da37faced697b318487dd2b92b54ba571bb
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:51:31 +1200

    August 25, 2021 8:51 AM

--- AskQuestion-3.py
+++ AskQuestion-3.py
@@ -1,6 +1,6 @@
 import random
 from checkscores1 import check_score
-from PlayerInformation-1 import PlayerInformation
+from PlayerInformation-1 import playerInformation
 global rounds
 points = 1
 rounds = 0
