08e2689db233689a02158903c03ccfdc5b2404c3
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 08:01:57 +1200

    August 25, 2021 8:01 AM

--- Scoreboard.py
+++ Scoreboard.py
@@ -12,7 +12,7 @@ def add_to_Scoreboard(string):
     with open("./Scores.txt", "a+") as scores:
         for line in scores:
             list.append(line)
-        print(line)
+        print(list)
 
         scores.seek(0)
 add_to_Scoreboard("23 - Michael")
