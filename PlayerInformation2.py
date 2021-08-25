6653dcefe1f9be1e2ca3c6ff2650b43c2b0a2a80
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 14:20:49 +1200

    August 25, 2021 2:20 PM

--- PlayerInformation2.py
+++ PlayerInformation2.py
@@ -9,6 +9,13 @@ def playerInformation():
     while name == "":
         name = input("Please enter a valid name: ")
         name = cleaninput(name)
+print("Hello, " + name.capitalize())
+   print("Welcome to my maths quiz")
+   print("\nImportant information:\n")
+   print("Your score goes up everytime you get a correct answer")
+   print("and goes down everytime you get a incorrect answer")
+   print("The questions will get harder as you play.")
+   print("If you have to leave the quiz just enter 'exit' into a question")
 
     difficulty = input("What difficulty would you like to play with?\nEasy or Hard: ").capitalize()
 
@@ -21,10 +28,3 @@ def playerInformation():
     return name.capitalize(), difficulty
 
 
-print("Hello, " + name.capitalize())
-   print("Welcome to my maths quiz")
-   print("\nImportant information:\n")
-   print("Your score goes up everytime you get a correct answer")
-   print("and goes down everytime you get a incorrect answer")
-   print("The questions will get harder as you play.")
-   print("If you have to leave the quiz just enter 'exit' into a question")
