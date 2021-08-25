1544bf6d1a8ef82d27450f6427d8893f090490c6
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 14:53:19 +1200

    August 25, 2021 2:53 PM

--- fully working program 1..py
+++ fully working program 1..py
@@ -30,7 +30,6 @@ def finish():
     global rounds
     print("your score was: ", points - 1)
     print(f"you lasted {rounds} rounds")
-    add_to_Scoreboard(f"{rounds} - {difficulty} - {name} ")
     exit()
 
 
