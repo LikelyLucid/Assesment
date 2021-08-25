832f3908fbea97d4c4ba14ebd71e5d83c1ce1cdc
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 14:52:46 +1200

    August 25, 2021 2:52 PM

--- fully working program 1..py
+++ fully working program 1..py
@@ -61,8 +61,8 @@ def answerwrong(points):
 while True:
     if difficulty == "Easy":
         # Set numbers to random a random number
-        num1 = random.randint(easymin, easymax + (rounds * 3))
-        num2 = random.randint(easymin, easymax + (rounds * 3) )
+        num1 = random.randint(easymin, easymax)
+        num2 = random.randint(easymin, easymax)
         # Make a choice between + or -
         choice = random.randint(1, 2)
 
