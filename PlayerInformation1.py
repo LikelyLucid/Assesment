47b29b9495224444c498d525d6c2d2a64673ad94
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 17:39:06 +1200

    August 25, 2021 5:39 PM

--- PlayerInformation1.py
+++ PlayerInformation1.py
@@ -2,6 +2,8 @@
 # Ask the user for their name and difficulty
 from RemovePunctuation import cleaninput
 import string
+
+
 def playerInformation():
     name = input("What is your name? ")
     name = cleaninput(name)
@@ -10,12 +12,13 @@ def playerInformation():
         name = input("Please enter a valid name: ")
         name = cleaninput(name)
 
-    difficulty = input("What difficulty would you like to play with?\nEasy or Hard: ").capitalize()
-
+    difficulty = input(
+        "What difficulty would you like to play with?\nEasy or Hard: ").capitalize()
 
     # Check if the user has entered easy or hard. if not, ask them again.
     while True:
         if difficulty in ["Easy", "Hard"]:
             break
-        difficulty = input("Please enter a difficulty\nEasy or Hard: ").capitalize()
+        difficulty = input(
+            "Please enter a difficulty\nEasy or Hard: ").capitalize()
     return name.capitalize(), difficulty
