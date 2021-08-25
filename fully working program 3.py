b396ea91a1af462e4d397df6d8d146e7a7a5a39a
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 16:55:13 +1200

    August 25, 2021 4:55 PM

--- fully working program 3.py
+++ fully working program 3.py
@@ -53,7 +53,7 @@ def answerright(points):
 
 def answerwrong(points):
     global rounds
-    playsound(INCORRECTSOUND, T)
+    playsound(INCORRECTSOUND, True)
     print("answer wrong")
     print(f"The answer was {answer}")
     points = points - 1  # add 1 to score
