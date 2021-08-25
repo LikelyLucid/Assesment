bf1d538eb28e1c9127136b29e5dc62de9f2ad5a9
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 17:39:03 +1200

    August 25, 2021 5:39 PM

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,37 +1,41 @@
-global list 
-list= []
+global list
+list = []
+
+
 def get_first_value(string):
     # print(string.split("-")[0])  #for testing to see the output
     return int(string.split("-")[0])
 
 
 def emptyfile(file):
-    open(file, "w").close # opens a file in write mode to delete all contents and closes it
+    # opens a file in write mode to delete all contents and closes it
+    open(file, "w").close
+
 
 def SortScores():
     # print(sorted(list, key=get_first_value))
     return sorted(list, key=get_first_value, reverse=True)
 
+
 def printscores():
     print("Score board:")
-    with open("./Scores.txt", "r") as scores: #open Scores.txt in read mode so it doesn't change the value on accident
+    # open Scores.txt in read mode so it doesn't change the value on accident
+    with open("./Scores.txt", "r") as scores:
         for line in scores:
-            print(line.replace("\n", "")) # remove \n and print it
+            print(line.replace("\n", ""))  # remove \n and print it
+
 
 def add_to_Scoreboard(string):
-    with open("./Scores.txt", "r+") as scores: #open file in read and append mode
+    with open("./Scores.txt", "r+") as scores:  # open file in read and append mode
         for line in scores:
-            list.append(line.replace("\n", "")) #replace \n in line with nothing
-        list.append(string) 
+            # replace \n in line with nothing
+            list.append(line.replace("\n", ""))
+        list.append(string)
     emptyfile("./Scores.txt")
     sortedlist = SortScores()
-    
+
     with open("./Scores.txt", "a") as scores:
         for i in sortedlist:
-            scores.write(i + "\n") #add the line and a \n to make the next item on another line
+            # add the line and a \n to make the next item on another line
+            scores.write(i + "\n")
     printscores()
-
-
-
-
-
