11ef1728c0ce92038fbd2b99b0b5b773a74da518
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 15:20:01 +1200

    August 25, 2021 3:20 PM

--- fully working program 1..py
+++ fully working program 1..py
@@ -124,7 +124,7 @@ while True:
             else:
                 points = answerwrong(points)
 
-        elif c
+        elif choice == 3:
         else:
     
             # figure out the answer
