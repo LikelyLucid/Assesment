719ec3686a29dcab0ef33dd4fe4278600c4fcdfd
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 16:49:57 +1200

    August 25, 2021 4:49 PM

--- fully working program 3.py
+++ fully working program 3.py
@@ -41,6 +41,7 @@ def finish():
 
 def answerright(points):
     global rounds
+    playsound()
     print("Correct answer!")
     points = points + 1  # add 1 to score
     # minus 1 point because they start with 1 point,
