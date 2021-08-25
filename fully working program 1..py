8eacdf7f92a081f6e5f2b860f6097b6fdf231aac
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 14:57:40 +1200

    August 25, 2021 2:57 PM

--- fully working program 1..py
+++ fully working program 1..py
@@ -26,7 +26,7 @@ def integer_checker(question):
 
 def finish():
     print("you lost")
-    print("your score was: {}".format(score))
+    print("your score was: {}".format(points))
 
 
 def answerright(points):
