55be161ad5d97744f26aebee80dc19cd4a995afb
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 17:39:48 +1200

    August 25, 2021 5:39 PM

--- fully working program 3.py
+++ fully working program 3.py
@@ -141,7 +141,7 @@ while True:
             else:
                 points = answerwrong(points)
         else:
-            max = 1 + rounds * 3  # change the difficulty of the numbers because
+            max = 1 + rounds*3  # change the difficulty of the numbers because
             # when testing they were to hard to solve,
             num1 = random.randint(1, max)
             # mostly because you had to times and divide in the hundreds
