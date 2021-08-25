0afc5d5a6793020719af2221ab1a4c492237389e
PurelyStupidity <micoolplays@gmail.com>
2021-08-25 16:59:05 +1200

    August 25, 2021 4:59 PM

--- fully working program 3.py
+++ fully working program 3.py
@@ -1,6 +1,6 @@
 import random
 import string
-import Threading
+import threading
 from playsound import playsound
 from checkscores1 import check_score
 from PlayerInformation2 import playerInformation
